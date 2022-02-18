83e45e7ad51cdb99b4e6f4cd54d671e03d8c7d9c
asenkyrik <asenkyrik@monetplus.cz>
2022-02-18 14:31:24 +0100

    Fixed maven dependencies. Run with "mvn clean javafx:run".

--- CoinApocalypse/src/main/java/CoinApocalypse/game/entities/Collisionable.java
+++
@@ -1,8 +0,0 @@
-package CoinApocalypse.game.entities;
-
-import javafx.geometry.Rectangle2D;
-
-public interface Collisionable {
-	public Collisionable intersects(Rectangle2D r);
-	public String getName();
-}
